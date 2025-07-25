2164c06bce8b5cc57042acbdc9414bc80337c095
Gabriel Daiki <daiki.gab@gmail.com>
2025-07-25 08:32:23 -0300

    added: add frined to list function with validations

---
+++ README.md
@@ -0,0 +1,6 @@
+# Challenge amigo secreto
+# :ghost: Funções
+- `adicionarAmigoNaLista`: Adiciona um nome na lista se ele for válido - não é vazio, ou espaços vazios - e remove espaços vazios desnecessários!
+- `Validar entrada`:
+- `Visualizar lista  de nomes`:
+- `Sorteio`:
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+let inputDeAmigo = document.querySelector('input');
+
+function adicionarAmigoNaLista(amigo) {
+    if (inputDeAmigo.value.trim() === '') {
+        alert("Input inválido!")
+        return adicionarAmigo
+    } else {
+        listaDeAmigos.push(amigo.trim());
+        limparCampo(inputDeAmigo);
+    }
+}
+
+function adicionarAmigo() {
+    adicionarAmigoNaLista(inputDeAmigo.value);
+    console.log(listaDeAmigos);
+}
+
+function limparCampo(elemento) {
+    elemento.value = '';
+}
